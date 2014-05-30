4f2b0429f7c0eac83893d6c01daf0a1fa34f5819
sielaq <sielaq@gmail.com>
2014-05-30 21:55:28 +0200

    Basic corrections for PoC

--- README.md
+++ README.md
@@ -3,13 +3,36 @@ fast-copy
 
 Fast Cascade Copy script that use split streams and basic Unix tools to speed up: pigz | tar | netcat | screen | mkfifo
 
-<img src="https://s3.amazonaws.com/easel.ly/all_easels/19186/FastCascadeCopy/image.jpg">
+<img src="http://s3.amazonaws.com/easel.ly/all_easels/19186/FastCascadeCopy/image.jpg">
+
+### Restrictions:
+
+#### 0) Remote Secure Shell and sudo 
+
+Script connect to each host using SSH, on which setting up (execute) specific tasks with sudo.
+That means you need to settup SSH keys, and configure sudoers on all hosts.
+
+#### 1) Required tools
+
+nc     - Till now it supports traditional nc (netcat)
+mkfifo - Needed for creating a splitted stream
+pigz   - A parallel implementation of gzip for modern multi-processor, multi-core machines.
+screen - For detaching background jobs
+
+#### 2) Destination directory must be empty or not exist
+
+This restriction exists for two reasons:
+1. Safety reason - nothing gonna be overwritten if you made mistake
+2. It would be harder to implement check sum verificaton
 
 ### Usage
 
 ```
-usage: -s|--source HOST:/path
-       -d|--destination HOST_A:/pash [HOST_B:/path]...[HOST_N:/path]
-       -p|--port 2222 (default 2222)
-ex.: fcp.sh -s dbmaster:/var/lib/mysql/data -d dblag:/var/lib/mysql/data db:/var/lib/mysql/data
+usage: -s|--source HOST_0:/path
+       -d|--destination HOST_1:/path [HOST_2:/path]...[HOST_N:/path]
+       -p|--port 2222            default is 2222
+       -v|--verify               verify if sum controls match on all hosts
+       -o|--only_verify          only verify without copying
+
+ex.: fcp.sh -v -s dbmaster:/var/lib/mysql/data -d dblag:/var/lib/mysql/data db:/var/lib/mysql/data
 ```
--- fcp.sh
+++ fcp.sh
@@ -7,6 +7,8 @@ declare -a DEST
 SSH="ssh -q -o StrictHostKeyChecking=no -o TCPKeepAlive=yes "
 NC="/bin/nc.traditional"
 CHKSUM="md5sum"
+VERIFY=1
+COPY=0
 
 RED="\033[31m"
 GRN="\033[32m"
@@ -24,10 +26,13 @@ NOK="$RED$NOK$END"
 
 
 display_help(){
- echo "$(basename $0) is a really fast copy between multiple hosts, using streams (fifo and pipes)"
- echo "usage: -s|--source HOST:/path"
- echo "       -d|--destination HOST_A:/pash [HOST_B:/path]...[HOST_N:/path]"
- echo "       -p|--port 2222 (default 2222)"
+ echo "$(basename $0) is a really fast cascade copy between multiple hosts, using streams (fifo and pipes)"
+ echo "restrictions: host cannot be repeated, destinatin directory must be empty"
+ echo "usage: -s|--source HOST_0:/path"
+ echo "       -d|--destination HOST_1:/path [HOST_2:/path]...[HOST_N:/path]"
+ echo "       -p|--port 2222            default is 2222"
+ echo "       -v|--verify               verify if sum controls match on all hosts"
+ echo "       -o|--only_verify          only verify without copying" 
  echo "ex.: $(basename $0) -s dbmaster:/var/lib/mysql/data -d dblag:/var/lib/mysql/data db:/var/lib/mysql/data"
 }
 
@@ -37,7 +42,7 @@ while :
 do
   case "$1" in
     -d | --dst|--destination)
-        shift 
+        shift
         while [ "${1+defined}" ] && [[ "${1}" != "-"* ]]; do
           DEST[$C]=$1
           C=$(($C+1))
@@ -45,7 +50,7 @@ do
         done
         ;;
     -s | --src|--source)
-        shift 
+        shift
 	HOST_SRC=$(echo $1|awk -F\: '{print $1}')
 	DIR_SRC=$(echo $1|awk -F\: '{print $2}')
         shift
@@ -57,6 +62,15 @@ do
     -p | --port)
         shift
         PORT=$1
+        ;;
+    -v | --verify)
+        shift
+        VERIFY=0
+        ;;
+    -o | --only_verify)
+        shift
+        VERIFY=0
+        COPY=1
         ;;
     --) # End of all options
         shift
@@ -77,102 +91,106 @@ app_check() {
 }
 
 check() {
-  EXIT=0
+  EXIT_CODE=0
   echo "$1:"
   APPS=$($SSH $1 "which pigz;which nc;which tar;which screen;ls -A $2|wc -l")
   COUNT_DIR=$(echo "${APPS}" | tail -n1)
-  app_check "${APPS}" pigz  || EXIT=1
-  app_check "${APPS}" nc    || EXIT=1
-  app_check "${APPS}" tar   || EXIT=1
-  app_check "${APPS}" screen|| EXIT=1
+  app_check "${APPS}" pigz  || EXIT_CODE=1
+  app_check "${APPS}" nc    || EXIT_CODE=1
+  app_check "${APPS}" tar   || EXIT_CODE=1
+  app_check "${APPS}" screen|| EXIT_CODE=1
   [ ! -z $2 ] && {
     [ "$COUNT_DIR" -eq 0 ] && echo -e "$OK destination dir $2 is empty" || {
-      echo -e "${NOK} destination dir $2 not empty"; EXIT=1;
+      echo -e "${NOK} destination dir $2 not empty"; EXIT_CODE=1;
     }
   }
-  return $EXIT
+  return ${EXIT_CODE}
 }
 
 check_sum() {
-  $SSH $1 "sudo bash -c \"cd $2;find . -type f | xargs -n1 -P4 $CHKSUM|sort -dk2|$CHKSUM\""
+  $SSH $1 "sudo sh -c \"cd $2;find . -type f | xargs -n1 -P4 $CHKSUM|sort -dk2|$CHKSUM\""
 }
 
-#1. check each host if contains needed apps
-EXIT=0
-check $HOST_SRC || EXIT=1
-for i in ${DEST[@]} 
-do
-  HOST=$(echo $i|awk -F\: '{print $1}')
-  DIR=$(echo $i|awk -F\: '{print $2}')
-  check $HOST $DIR || EXIT=1
-done
-[ $EXIT -eq 1 ] && exit 1
-
-echo "Start copying..."
-
-# 2. Install on last host screen ith netcat
-HOST_FIRST=$(echo ${DEST[0]}|awk -F\: '{print $1}')
-HOST_LAST=$(echo ${DEST[$C-1]}|awk -F\: '{print $1}')
-DIR_LAST=$(echo ${DEST[$C-1]}|awk -F\: '{print $2}')
-$SSH -f $HOST_LAST "sudo screen -S fcp.$$.C -dm sh -c \"[ -d ${DIR_LAST} ] || mkdir -p ${DIR_LAST};${NC} -l -p ${PORT} -q 0 | pigz -d| tar xvf - -C ${DIR_LAST}\""
-
-# 3. For all hosts in between first and last, install pipe splitter 
-ELEM_LAST=""
-if [ ${#DEST[@]} -gt 1 ]
-then
+[ ${COPY} -eq 0 ] && {
+  #1. check each host if contains needed apps
+  EXIT=0
+  echo "Checking all hosts for required tools..."
+  check $HOST_SRC || EXIT=1
+  for i in ${DEST[@]}
+  do
+    HOST=$(echo $i|awk -F\: '{print $1}')
+    DIR=$(echo $i|awk -F\: '{print $2}')
+    check $HOST $DIR || EXIT=1
+  done
+  [ ${EXIT} -eq 1 ] && exit 1
+  
+  # 2. Install on last host screen with netcat
+  echo "Setting up last host..."
+  HOST_LAST=$(echo ${DEST[$C-1]}|awk -F\: '{print $1}')
+  DIR_LAST=$(echo ${DEST[$C-1]}|awk -F\: '{print $2}')
+  $SSH -f $HOST_LAST "sudo screen -S fcp.$$.C -dm sh -c \"[ -d ${DIR_LAST} ] || mkdir -p ${DIR_LAST};${NC} -l -p ${PORT} -q 0 | pigz -d| tar xvf - -C ${DIR_LAST}\""
+  
+  # 3. For all hosts in between first and last, install pipe splitter
+  echo "Setting up splitter..."
+  ELEM_LAST=""
+  if [ ${#DEST[@]} -gt 1 ]
+  then
+    for i in "${DEST[@]}"
+    do
+      [ ! -z $ELEM_LAST ] && {
+        HOST_NEXT=$(echo $i|awk -F\: '{print $1}')
+        HOST=$(echo ${ELEM_LAST}|awk -F\: '{print $1}')
+        DIR=$(echo ${ELEM_LAST}|awk -F\: '{print $2}')
+        echo "$HOST - setting up fifo and splitter"
+        $SSH -f $HOST "sudo screen -S fcp.$$.A -dm sh -c \"mkfifo /tmp/myfifo; ${NC} -q 0 ${HOST_NEXT} ${PORT} </tmp/myfifo ; sleep 1d \""
+        $SSH -f $HOST "sudo screen -S fcp.$$.B -dm sh -c \"[ -d ${DIR} ] || mkdir -p ${DIR};${NC} -l -p ${PORT} -q 0|tee /tmp/myfifo|pigz -d|tar xvf - -C ${DIR}\""
+      }
+      ELEM_LAST=$i
+    done
+  fi
+  
+  # 4. Start copy
+  HOST_FIRST=$(echo ${DEST[0]}|awk -F\: '{print $1}')
+  echo "Start copying..."
+  $SSH $HOST_SRC "sudo sh -c \"tar cv -C ${DIR_SRC} .| pigz | ${NC} -q 0 ${HOST_FIRST} ${PORT}\" " || {
+    echo "Copy error"
+    exit 1
+  }
+  
+  # 5. Clean up - remove fifo(s) and screen(s)
+  echo "Clean up..."
   for i in "${DEST[@]}"
   do
-    [ ! -z $ELEM_LAST ] && {
-      HOST_NEXT=$(echo $i|awk -F\: '{print $1}')
-      HOST=$(echo ${ELEM_LAST}|awk -F\: '{print $1}')
-      DIR=$(echo ${ELEM_LAST}|awk -F\: '{print $2}')
-      echo "$HOST - setting up fifo and splitter" 
-      $SSH -f $HOST "sudo screen -S fcp.$$.A -dm sh -c \"mkfifo /tmp/myfifo; ${NC} -q 0 ${HOST_NEXT} ${PORT} </tmp/myfifo ; sleep 1d \""
-      $SSH -f $HOST "sudo screen -S fcp.$$.B -dm sh -c \"[ -d ${DIR} ] || mkdir -p ${DIR};${NC} -l -p ${PORT} -q 0|tee /tmp/myfifo|pigz -d|tar xvf - -C ${DIR}\"" 
-    }
-    ELEM_LAST=$i
+    HOST=$(echo $i|awk -F\: '{print $1}')
+    $SSH -f $HOST "sudo rm /tmp/myfifo >/dev/null 2>&1;sudo screen -X -S fcp.$$ quit >/dev/null 2>&1"
   done
-fi
-
-# 4. Start copy
-$SSH $HOST_SRC "sudo bash -c \"tar cv -C ${DIR_SRC} .| pigz | ${NC} -q 0 ${HOST_FIRST} ${PORT}\" " || {
-  echo "Copy error"
-  exit 1
 }
 
-# 5. Clean up
-echo "Cleanup - remove fifo(s) and screen(s)"
-for i in "${DEST[@]}"
-do
-  HOST=$(echo $i|awk -F\: '{print $1}')
-  $SSH -f $HOST "sudo rm /tmp/myfifo >/dev/null 2>&1;sudo screen -X -S fcp.$$ quit >/dev/null 2>&1" 
-done
-
 #6. Verify
+[ ${VERIFY} -eq 0 ] && {
+  echo "Calculating sum controls, please wait..."
+  check_sum ${HOST_SRC} ${DIR_SRC} > /tmp/$$.$HOST_SRC &
+  for i in "${DEST[@]}"
+  do
+    HOST=$(echo $i|awk -F\: '{print $1}')
+    DIR=$(echo $i|awk -F\: '{print $2}')
+    check_sum ${HOST} ${DIR} > /tmp/$$.$HOST &
+  done
+  wait
+  
+  SRC_SUM=$(cat /tmp/$$.$HOST_SRC)
+  echo -e "${HOST_SRC}: \t${SRC_SUM}"
+  for i in "${DEST[@]}"
+  do
+    HOST=$(echo $i|awk -F\: '{print $1}')
+    HOST_SUM=$(cat /tmp/$$.$HOST)
+    echo -e "${HOST}: \t${HOST_SUM}"
+    [ "$SRC_SUM" = "$HOST_SUM" ] && echo -e "$OK all $CHKSUM from $DIR are identical like on ${HOST_SRC}:${DIR_SRC}" || {
+      echo -e "$NOK some $CHKSUM from $DIR are different than ${HOST_SRC}:${DIR_SRC}"
+    }
+  done
+  
+  rm /tmp/$$.*
+}
 
-check_sum ${HOST_SRC} ${DIR_SRC} > /tmp/$$.$HOST_SRC &
-for i in "${DEST[@]}"
-do
-  HOST=$(echo $i|awk -F\: '{print $1}')
-  DIR=$(echo $i|awk -F\: '{print $2}')
-  check_sum ${HOST} ${DIR} > /tmp/$$.$HOST &
-  echo "in loop"
-done
-
-echo wait
-wait
-
-SRC_SUM=$(cat /tmp/$$.$HOST_SRC)
-echo -e "${HOST_SRC}: \t${SRC_SUM}"
-for i in "${DEST[@]}"
-do
-  HOST=$(echo $i|awk -F\: '{print $1}')
-  HOST_SUM=$(cat /tmp/$$.$HOST)
-  echo -e "${HOST}: \t${HOST_SUM}"
-  [ "$SRC_SUM" = "$HOST_SUM" ] && echo -e "$OK all $CHKSUM from $DIR are identical like on ${HOST_SRC}:${DIR_SRC}" || {
-    echo -e "$NOK some $CHKSUM from $DIR are different than ${HOST_SRC}:${DIR_SRC}"
-  }
-done
-
-rm /tmp/$$.*
 exit 0
